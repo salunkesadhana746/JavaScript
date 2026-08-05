2d3b342f9ab2a2829aca02ce6ecfdee17f3bc644
salunkesadhana746 <salunkesadhana746@gmail.com>
2026-08-06 00:16:43 +0530

    ...

---
+++ day 2/relational operators/script.js
@@ -0,0 +1,26 @@
+//Equal to (==)
+
+let a = 20;
+let b = 30;
+
+console.log("a == b",a == b);
+
+// not equal to (!=)
+
+console.log("a != b",a != b);
+
+// Greater than (>)
+
+console.log("a > b",a > b);
+
+// Greater than or equal (>=)
+
+console.log("a >= b",a >= b);
+
+// less than (<)
+
+console.log("a < b",a < b);
+
+// less than or equal (<=)
+
+console.log("a <= b",a <= b);
