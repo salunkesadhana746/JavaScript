cdcc374549c27849b66e96bedb4ae1aac322e161
salunkesadhana746 <salunkesadhana746@gmail.com>
2026-08-06 11:40:20 +0530

    ...

---
+++ day 2/Logical operators/script.js
@@ -0,0 +1,13 @@
+//Logical operators
+
+//&& - AND: Returns true only if all parts are true.
+//|| - OR: Returns true if at least one part is true.
+// (!)-NOT: Changes true to false, and false to true.
+
+let a = 20;
+let b = 30;
+let c = 50 ;
+
+console.log(a < b && b < c);
+console.log(a > b || b < c);
+console.log(!(a <b));
