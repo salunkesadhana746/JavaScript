b1c38cecc6776c734fbaad9185b10fbd2ce62516
salunkesadhana746 <salunkesadhana746@gmail.com>
2026-08-07 12:34:45 +0530

    ...

---
+++ conditional statements/numbers/script.js
@@ -0,0 +1,13 @@
+//wap to check number is negitive, positive, neutral
+
+let num = 0;
+
+if(num > 0){
+    console.log("number is positive !");
+}
+else if(num == 0){
+    console.log("number is neutral !");
+}
+else{
+    console.log("number is negitive !");
+}
---
+++ conditional statements/voting/script.js
@@ -0,0 +1,10 @@
+// voting
+
+let age = 18;
+
+if(age >= 18){
+    console.log("you are eligible for vote !");
+}
+else{
+    console.log("you are not eligible for vote !");
+}
