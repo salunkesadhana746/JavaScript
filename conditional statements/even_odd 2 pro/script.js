920af2cc802d35478b31f96d025a5c0f95179c0e
salunkesadhana746 <salunkesadhana746@gmail.com>
2026-08-08 00:34:57 +0530

    ...

---
+++ conditional statements/even_odd 2 pro/script.js
@@ -0,0 +1,10 @@
+// wap to check number is even or odd using if..else stastment without using %
+
+let num = 11;
+
+if((num & 1) == 0){
+    console.log("number is even !");
+}
+else{
+    console.log("number is odd !");
+}
