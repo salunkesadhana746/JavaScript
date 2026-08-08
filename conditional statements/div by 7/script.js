92a1216559892a94804cb4dc99cc184f5a472200
salunkesadhana746 <salunkesadhana746@gmail.com>
2026-08-08 13:10:53 +0530

    ...

---
+++ conditional statements/div by 7/script.js
@@ -0,0 +1,10 @@
+//wap to check if a number is divisible by 7
+
+let num = 14;
+
+if(num % 7 == 0){
+    console.log(num,"number is divisible by 7");
+}
+else{
+    console.log(num,"number is not divisible by 7");
+}
---
+++ conditional statements/student grades/script.js
@@ -0,0 +1,25 @@
+//wap to grades of student based on their percentage 
+
+let percentage = 40.20;
+
+if(percentage >= 95){
+    console.log("Your Grade A+");
+}
+else if(percentage >= 90){
+    console.log("Your Grade A");
+}
+else if(percentage >= 80){
+    console.log("Your Grade B+");
+}
+else if(percentage >= 70){
+    console.log("Your Grade B");
+}
+else if(percentage >= 50){
+    console.log("Your Grade C");
+}
+else if(percentage >= 40){
+    console.log("Your Grade D");
+}
+else{
+    console.log("Your Fail !");
+}
