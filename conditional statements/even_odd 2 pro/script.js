d6f2aa44d90236083f877e905fb8ff4bb607c1b2
salunkesadhana746 <salunkesadhana746@gmail.com>
2026-08-11 10:50:16 +0530

    ...

--- conditional statements/even_odd 2 pro/script.js
+++ conditional statements/even_odd 2 pro/script.js
@@ -1,10 +1,19 @@
 // wap to check number is even or odd using if..else stastment without using %
 
-let num = 11;
+let num = 12;
 
-if((num & 1) == 0){
-    console.log("number is even !");
-}
-else{
-    console.log("number is odd !");
-}
+//if((num & 1) == 0){
+ //   console.log("number is even !");
+//}
+//else{
+//    console.log("number is odd !");
+//}
+
+if (Math.floor(num / 2) * 2 == num) 
+    {
+    console.log(num + " is Even Number !");
+    } 
+    else 
+    {
+    console.log(num + " is Odd Number !");
+    }
